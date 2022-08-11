a41e0ced1c05bebb7f126c8de11456fe92030369
Luiz Felipe Romani <luiz.tobaldini@cooperalfa.coop.br>
2022-08-11 13:10:32 -0300

    update chamado implementado

--- src/main/java/com/luizfelipe/obra/services/ObraService.java
+++ src/main/java/com/luizfelipe/obra/services/ObraService.java
@@ -35,6 +35,14 @@ public class ObraService {
 		return obraRepository.save(newObra(objDTO));
 	}
 	
+
+	public Obra update(Long id, @Valid ObraDTO objDTO) {
+		objDTO.setId(id);
+		Obra oldObj = findById(id);
+		oldObj = newObra(objDTO);
+		return obraRepository.save(oldObj);
+	}
+	
 	private Obra newObra(ObraDTO obj) {
 		Cliente cliente = clienteService.findById(obj.getIdCliente());
 		
