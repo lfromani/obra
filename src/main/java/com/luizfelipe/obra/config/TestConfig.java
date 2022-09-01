66eae38bd3ee0e6516e6e3e4c7b50db94018334a
lfromani <luizfelipe.fe@gmail.com>
2022-08-31 22:13:17 -0300

    Agora vai

--- src/main/java/com/luizfelipe/obra/config/TestConfig.java
+++ src/main/java/com/luizfelipe/obra/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.luizfelipe.obra.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,9 +15,14 @@ public class TestConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
 	public void instanciaDB() {
-		this.dbService.instanciaDB();
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
 	}
 
 }
